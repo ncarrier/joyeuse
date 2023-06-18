db50294dbd369e54527271d05d853341f84d6cea
Nicolas Carrier <carrier.nicolas0@gmail.com>
2023-06-18 15:37:43 +0200

    import du contenu de la conteuse

---
+++ examples/copie/Secrets/info.js
@@ -0,0 +1,13 @@
+var info = {
+ VERSION: 'V05.07',
+ SERIAL_NUMBER: 'JOY_4B004D0008PRXW48X_1',
+ BABY_MODE: 'N',
+ LEGACY_HW: 'N',
+ BILINGUAL_MODE: 'N',
+ FR: 'Y',
+ EN: 'N',
+ DE: 'N',
+ IT: 'N',
+ INTERNATIONAL_MODE: 'N'
+
+};
