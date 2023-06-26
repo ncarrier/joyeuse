35529b60d6eb1e8cd84284fc95778ec43784929a
Nicolas Carrier <carrier.nicolas0@gmail.com>
2023-06-26 03:29:42 +0200

    not tested exe generation script for windows

---
+++ .github/scripts/build-win.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# shamelessly stolen and adapted from:
+#  https://github.com/Mord3rca/gamma-launcher/blob/master/.github/scripts/release.sh
+
+SCRIPT_NAME="${PWD}/joyeuse.py"
+cat >"${SCRIPT_NAME}" <<EOF
+import re
+import sys
+from launcher.cli import main
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
+    sys.exit(main())
+EOF
+
+pyinstaller --noconsole \
+	--paths . --onefile --name joyeuse \
+	--exclude-module _bootlocale \
+	--hidden-import=tkinter \
+	"${SCRIPT_NAME}"
+
