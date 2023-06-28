855e40efd8db023d93b7832c7d763d1a4e892ed2
Nicolas Carrier <carrier.nicolas0@gmail.com>
2023-06-28 09:47:51 +0200

    use the same entry script for local and github windows builds

--- .github/scripts/build-win.sh
+++ .github/scripts/build-win.sh
@@ -2,21 +2,10 @@
 # shamelessly stolen and adapted from:
 #  https://github.com/Mord3rca/gamma-launcher/blob/master/.github/scripts/release.sh
 
-SCRIPT_NAME="${PWD}/joyeuse.py"
-cat >"${SCRIPT_NAME}" <<EOF
-import re
-import sys
-from joyeuse.__main__ import main
-if __name__ == '__main__':
-    sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
-    sys.exit(main())
-EOF
-
 pyinstaller --noconsole \
 	--paths . --onefile --name joyeuse \
 	--exclude-module _bootlocale \
 	--hidden-import=tkinter \
 	--hidden-import=pillow \
 	--collect-submodules=pillow \
-	"${SCRIPT_NAME}"
-
+	"${PWD}/script/joyeuse.py"
