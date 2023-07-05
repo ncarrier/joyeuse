1419301f34fe5f137c5dda7e1d627f7e1722199c
Nicolas Carrier <carrier.nicolas0@gmail.com>
2023-07-05 07:12:47 +0200

    pep8 in tk_video_player snippet

--- snippets/video/tk_video_player.py
+++ snippets/video/tk_video_player.py
@@ -4,9 +4,10 @@ from tkVideoPlayer import TkinterVideo
 root = tk.Tk()
 
 videoplayer = TkinterVideo(master=root, scaled=True)
-videoplayer.load("/home/nicolas/workspace/joyeuse/examples/copie/Secrets/Tutos vidéo/1-Allumer la Conteuse.mp4")
+file = "../../examples/copie/Secrets/Tutos vidéo/1-Allumer la Conteuse.mp4"
+videoplayer.load(file)
 videoplayer.pack(expand=True, fill="both")
 
-videoplayer.play() # play the video
+videoplayer.play()  # play the video
 
 root.mainloop()
